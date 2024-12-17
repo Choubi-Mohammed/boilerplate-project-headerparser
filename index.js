b96882db90fa69637a334dc3c1f3ba658ff452aa
Mohammed Choubi <choubimohammed99@gmail.com>
2024-12-17 16:09:13 +0000

    complete challange

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.get("/api/whoami",(req,res)=>{
+  const ipAdress=req.headers["x-forwarded-for"] || req.socket.remoteAddress
+  const language=req.headers["accept-language"]
+  const software=req.headers["user-agent"]
+  res.json({
+    ipAdress: ipAdress,
+    language: language,
+    software: software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
